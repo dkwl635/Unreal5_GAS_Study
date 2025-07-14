2a4e0f397ca4bd964d2cda6b37e5cae7d24b71c5
SeSAcPc_dkwl635 <dkwl635@naver.com>
2025-07-14 14:47:10 +0900

    +AuraUserWidget +AuraWidgetController

---
+++ Source/Aura/Public/UI/MyClass.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMyClass : public UObject
+{
+	GENERATED_BODY()
+	
+};
