3c36f36b615b54029bc48a1ca66f28cb7b3c9022
SeSAcPc_dkwl635 <dkwl635@naver.com>
2025-07-16 13:32:18 +0900

    +AuraHUD +BP_AuraHUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget =	CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+
+	if (Widget)
+	{
+		Widget->AddToViewport();
+	}
+}
